5b63b1a66ccf953e3895270dfb34dfb9e919ce1d
Guido Riembauer <riembauer@mundialis.de>
2022-01-24 16:53:40 +0100

    update copyright i.sentinel_1.mosaic

--- i.sentinel_1.mosaic/i.sentinel_1.mosaic.py
+++ i.sentinel_1.mosaic/i.sentinel_1.mosaic.py
@@ -11,9 +11,15 @@
 #
 # COPYRIGHT:	(C) 2020-2022 by mundialis GmbH & Co. KG and the GRASS Development Team
 #
-#               This program is free software under the GNU General Public
-#               License (>=v2). Read the file COPYING that comes with GRASS
-#               for details.
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
 #############################################################################
 
