ff38b58d4945cc914a622787e2a5f34b48849693
jhalbauer <j.halbauerjena@gmail.com>
2025-03-11 11:38:18 +0100

    added changes by GR

--- i.sentinel_1.forest.mask/i.sentinel_1.forest.mask.py
+++ i.sentinel_1.forest.mask/i.sentinel_1.forest.mask.py
@@ -2,12 +2,12 @@
 #
 ############################################################################
 #
-# MODULE:      i.forest.mask
+# MODULE:      i.sentinel_1.forest.mask
 # AUTHOR(S):   Johannes Halbauer
 #
 # PURPOSE:     Creates forest mask based on monthly Sentinel-1 mosaics
 #
-# COPYRIGHT:   (C) 2024 by mundialis GmbH & Co. KG and the GRASS
+# COPYRIGHT:   (C) 2025 by mundialis GmbH & Co. KG and the GRASS
 #              Development Team
 #
 # This program is free software; you can redistribute it and/or modify
@@ -31,20 +31,61 @@
 
 # %option G_OPT_V_INPUT
 # % key: aoi
-# % description: Vector map to restrict mask creation to
+# % description: Polygon of the area of interest to set computational region
 # % required: no
 # %end
 
 # %option G_OPT_R_INPUT
 # % key: sar_img_vv
-# % description: Raster map containing VV SAR image (db)
+# % description: Raster map containing VV gamma0 SAR image (db)
 # % required: yes
 # %end
 
 # %option G_OPT_R_INPUT
 # % key: sar_img_vh
-# % description: Raster map containing VH SAR image (db)
+# % description: Raster map containing VH gamma0 SAR image (db)
 # % required: yes
+# %end
+
+# %option
+# % key: vh_thresh
+# % description: Threshold marking lower border of forest-representing VH backscatter values to create core forest mask
+# % required: no
+# % answer: -28.0
+# %end
+
+# %option
+# % key: vv_vh_ratio_thresh
+# % description: Thresholds of forest-representing VV/VH ratio interval to create core forest mask
+# % required: no
+# % answer: 10.0,20.0
+# %end
+
+# %option
+# % key: rvi_thresh
+# % description: Threshold marking lower border of forest-representing RVI values to create core forest mask
+# % required: no
+# % answer: 0.2
+# %end
+
+# %option
+# % key: seg_threshold
+# % description: Distinction threshold used for image segmentation
+# % required: no
+# % answer: 0.017
+# %end
+
+# %option
+# % key: seg_iterations
+# % description: Maximum number of iteratons used for image segmentation
+# % required: no
+# % answer: 30
+# %end
+
+# %option
+# % key: seg_minsize
+# % description: Minimum cell number in a segment used for image segmentation
+# % required: no
 # %end
 
 # %option G_OPT_R_OUTPUT
@@ -57,11 +98,6 @@
 # % key: forest_mask_vect
 # % description: Name of output vector map containing the forest mask
 # % required: no
-# %end
-
-# %flag
-# % key: v
-# % description: Vectorize forest map
 # %end
 
 
@@ -96,6 +132,7 @@ def calc_vv_vh_ratio_and_rvi(vv_db, vh_db):
         vv_vh_ratio_lin (str): Name of VV/VH ratio (lin) raster map
         rvi_lin (str): Name of Radar Vegetation Index (lin) raster map
     """
+    grass.message(_("Calculate VV/VH ratio and Radar Vegetation Index...)"))
     # transform input SAR images to linear scale
     sar_img_vv_lin = f"sar_img_vv_lin_{ID}"
     sar_img_vh_lin = f"sar_img_vh_lin_{ID}"
@@ -129,7 +166,10 @@ def calc_vv_vh_ratio_and_rvi(vv_db, vh_db):
     # calculate Radar Vegetation Index (lin)
     grass.run_command(
         "r.mapcalc",
-        expression=f"{rvi_lin}=(4*{sar_img_vh_lin})/({sar_img_vv_lin}+{sar_img_vh_lin})",
+        expression=(
+            f"{rvi_lin}=(4*{sar_img_vh_lin})/"
+            f"({sar_img_vv_lin}+{sar_img_vh_lin})"
+        ),
         quiet=True,
     )
     rm_rast.append(rvi_lin)
@@ -137,24 +177,33 @@ def calc_vv_vh_ratio_and_rvi(vv_db, vh_db):
     return vv_vh_ratio_lin, rvi_lin
 
 
-def create_forest_segments(seg_group, core_forest_mask):
+def create_forest_segments(
+    seg_group, core_forest_mask, seg_thresh, seg_iter, seg_minsize
+):
     """Create segments and extract forest segments
     Args:
         seg_group (str): Name of raster map group to use for segmentation
         core_forest_mask (str): Name of pixel-based core forest mask
+        seg_thresh (float): Threshold used in i.segment
+        seg_thresh (int): Number of iterations used in i.segment
+        seg_minsize (int): Minimum number of cells in a segment used in i.segment
     Returns:
         forest_seg_vect (str): Name of forest segments vector map
     """
+    grass.message(
+        _("Perform image segmentation and determine forest segments...")
+    )
     # segmentation based on group
     segments = f"segements_{ID}"
-    grass.run_command(
-        "i.segment",
-        group=seg_group,
-        output=segments,
-        threshold=0.017,
-        iterations=30,
-        overwrite=True,
-    )
+    seg_params = {
+        "group": seg_group,
+        "output": segments,
+        "threshold": seg_thresh,
+        "iterations": seg_iter,
+    }
+    if seg_minsize:
+        segments["minsize"] = seg_minsize
+    grass.run_command("i.segment", **seg_params, overwrite=True, quiet=True)
     rm_rast.append(segments)
 
     # vectorize segments
@@ -165,6 +214,7 @@ def create_forest_segments(seg_group, core_forest_mask):
         output=vh_seg_vect,
         type="area",
         overwrite=True,
+        quiet=True,
     )
     rm_vec.append(vh_seg_vect)
 
@@ -176,6 +226,7 @@ def create_forest_segments(seg_group, core_forest_mask):
         column_prefix="core",
         method="number,sum",
         overwrite=True,
+        quiet=True,
     )
 
     # extract forest segments
@@ -186,6 +237,7 @@ def create_forest_segments(seg_group, core_forest_mask):
         output=forest_seg_vect,
         where="(core_sum / core_number) > 0.01",
         overwrite=True,
+        quiet=True,
     )
     rm_vec.append(forest_seg_vect)
 
@@ -198,6 +250,7 @@ def smooth_forest_mask(forest_seg_vect, final_forest_mask):
         forest_seg_vect (str): Name of forest segments vector map
         final_forest_mask (str): Name of resulting raster map
     """
+    grass.message(_("Smooth forest mask..."))
     # convert forest segments to raster
     forest_seg_rast = f"forest_seg_rast_{ID}"
     grass.run_command(
@@ -246,6 +299,17 @@ def main():
     sar_img_vv_db = options["sar_img_vv"]
     sar_img_vh_db = options["sar_img_vh"]
 
+    # set parameters
+    vh_thresh = options["vh_thresh"]
+    vv_vh_rat_thresh = tuple(options["vv_vh_ratio_thresh"].split(","))
+    rvi_thresh = options["rvi_thresh"]
+    seg_thresh = float(options["seg_threshold"])
+    seg_iter = int(options["seg_iterations"])
+    if options["seg_minsize"]:
+        seg_minsize = int(options["seg_minsize"])
+    else:
+        seg_minsize = None
+
     # set output variables
     final_forest_mask = options["forest_mask"]
 
@@ -253,16 +317,13 @@ def main():
         final_forest_mask_vect = options["forest_mask_vect"]
     else:
         final_forest_mask_vect = None
-    if flags["v"] and not final_forest_mask_vect:
-        grass.fatal(_("-v flag set, but option <forest_mask_vect> not given."))
 
     # set region
+    grass.run_command("g.region", save=ORIG_REGION, quiet=True)
     if aoi:
-        grass.run_command("g.region", save=ORIG_REGION)
-        grass.run_command("g.region", vector=aoi)
+        grass.run_command("g.region", vector=aoi, quiet=True)
     else:
-        grass.run_command("g.region", save=ORIG_REGION)
-        grass.run_command("g.region", raster=sar_img_vv_db)
+        grass.run_command("g.region", raster=sar_img_vv_db, quiet=True)
 
     # calculate VV/VH ratio and RVI
     vv_vh_ratio_lin, rvi_lin = calc_vv_vh_ratio_and_rvi(
@@ -271,10 +332,14 @@ def main():
 
     # calculate core forest mask
     core_forest_mask = f"core_forest_mask_temp_{ID}"
+    expr_str = (
+        f"{core_forest_mask}=if({sar_img_vh_db} > {vh_thresh} && "
+        f"{vv_vh_ratio_lin} > {vv_vh_rat_thresh[0]} && "
+        f"{vv_vh_ratio_lin} < {vv_vh_rat_thresh[1]} && "
+        f"{rvi_lin} > {rvi_thresh}, 1, 0)"
+    )
     grass.run_command(
-        "r.mapcalc",
-        expression=f"{core_forest_mask}=if({sar_img_vh_db} > -28 && {vv_vh_ratio_lin} > 10.0 && {vv_vh_ratio_lin} < 20.0 && {rvi_lin} > 0.2, 1, 0)",
-        quiet=True,
+        "r.mapcalc", expression=expr_str, overwrite=True, quiet=True
     )
     rm_rast.append(core_forest_mask)
 
@@ -284,18 +349,21 @@ def main():
         "i.group",
         group=seg_group,
         input=f"{sar_img_vh_db},{vv_vh_ratio_lin},{rvi_lin}",
+        overwrite=True,
         quiet=True,
     )
     rm_groups.append(seg_group)
 
     # perform segmentation and forest extraction
-    forest_seg_vect = create_forest_segments(seg_group, core_forest_mask)
+    forest_seg_vect = create_forest_segments(
+        seg_group, core_forest_mask, seg_thresh, seg_iter, seg_minsize
+    )
 
     # smooth raster forest segments
     smooth_forest_mask(forest_seg_vect, final_forest_mask)
 
     # convert forest mask raster map to vector map
-    if flags["v"]:
+    if final_forest_mask_vect:
         grass.run_command(
             "r.to.vect",
             input=final_forest_mask,
